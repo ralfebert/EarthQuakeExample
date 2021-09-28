70f4acecbff54d9312b60c2060b2246f315f76b8
Ralf Ebert <info@ralfebert.de>
2021-09-28 11:38:21 +0200

    starter

--- EarthquakeExample/ContentView.swift
+++ EarthquakeExample/ContentView.swift
@@ -3,49 +3,69 @@ import SwiftUI
 
 struct Earthquake: Identifiable {
     var id = UUID()
-    var date: Date
-    var location: CLLocationCoordinate2D
+    let date: Date
+    let location: CLLocationCoordinate2D
+    let depth: Decimal
+    let mag: Decimal
+
+    static let dateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions.insert(.withFractionalSeconds)
+        return formatter
+    }()
+
+    static let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.generatesDecimalNumbers = true
+        return formatter
+    }()
 
     init(line: String) {
-        let dateFormatter = ISO8601DateFormatter()
-        dateFormatter.formatOptions = [.withFractionalSeconds]
-
         let fields = line.split(separator: ",")
-        self.date = dateFormatter.date(from: String(fields[0]))!
-        self.location = CLLocationCoordinate2D(latitude: .init(fields[1])!, longitude: .init(fields[2])!)
+        date = Self.dateFormatter.date(from: String(fields[0]))!
+        location = CLLocationCoordinate2D(latitude: .init(fields[1])!, longitude: .init(fields[2])!)
+        depth = Self.numberFormatter.number(from: String(fields[2]))!.decimalValue
+        mag = Self.numberFormatter.number(from: String(fields[3]))!.decimalValue
     }
 }
 
 @MainActor
 class EarthquakesModel: ObservableObject {
-
     @Published var earthquakes = [Earthquake]()
 
     func load() async throws {
-        self.earthquakes = []
+        earthquakes = []
         let url = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv")!
-        for try await line in url.lines {
-            if !line.hasPrefix("time,") {
-                self.earthquakes.append(Earthquake(line: line))
-            }
-        }
     }
-
 }
 
 struct ContentView: View {
-
     @StateObject var earthquakesModel = EarthquakesModel()
 
     var body: some View {
-        VStack(spacing: 10) {
-            Text("\(earthquakesModel.earthquakes.count) earthquakes")
-            Button("Reload") {
+        NavigationView {
+            List(earthquakesModel.earthquakes) { earthquake in
+                VStack(alignment: .leading) {
+                    Text("\(earthquake.date, format: .dateTime)")
+                    Text("Magnitude: \(earthquake.mag, format: .number.precision(.significantDigits(2)))")
+                }
+            }
+            .navigationTitle("Earthquakes")
+            .navigationBarItems(trailing: self.navBarButtons)
+        }
+    }
+
+    @ViewBuilder var navBarButtons: some View {
+        Button(
+            action: {
                 Task {
                     try! await earthquakesModel.load()
                 }
+            },
+            label: {
+                Image(systemName: "arrow.clockwise")
             }
-        }
+        )
     }
 }
 
